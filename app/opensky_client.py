4f2f14087ad851cbfca64d10a129db16dc397251
Raul Valverde <l231080023@iztapalapa.tecnm.mx>
2025-11-06 12:41:58 -0600

    Consumo de api con mapa en la cdmx

--- app/opensky_client.py
+++ app/opensky_client.py
@@ -2,26 +2,22 @@ import requests
 
 BASE_URL = "https://opensky-network.org/api"
 
-def get_all_states(lamin=None, lomin=None, lamax=None, lomax=None):
-    """
-    Obtiene datos de vuelos actuales. Puede filtrar por área geográfica.
-    """
+def get_all_states(
+    lamin=18.8, lomin=-100.2, lamax=20.2, lomax=-98.6
+):
+    """Obtiene datos de vuelos actuales (por defecto zona CDMX)."""
     url = f"{BASE_URL}/states/all"
-    params = {}
-
-    if all(v is not None for v in [lamin, lomin, lamax, lomax]):
-        params.update({
-            "lamin": lamin,
-            "lomin": lomin,
-            "lamax": lamax,
-            "lomax": lomax
-        })  
+    params = {
+        "lamin": lamin,
+        "lomin": lomin,
+        "lamax": lamax,
+        "lomax": lomax
+    }
 
     try:
         response = requests.get(url, params=params, timeout=10)
         response.raise_for_status()
-        data = response.json()
-        return data
+        return response.json()
     except requests.RequestException as e:
         print(f"Error consultando OpenSky API: {e}")
         return None
